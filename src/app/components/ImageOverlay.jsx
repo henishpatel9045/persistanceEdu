051c7af630a4683d0b44ca5aee4b32c35a98a29a
henishpatel9045 <200120107182@git.org.in>
2023-03-11 22:12:03 +0530

    overlay centered

--- src/app/components/ImageOverlay.jsx
+++ src/app/components/ImageOverlay.jsx
@@ -5,7 +5,7 @@ import "./components.scss"
 function ImageOverlay({show, setShow, src}) {
     return (
         <Modal show={show} size="xl" onHide={() => setShow(false)}>
-            <Modal.Body className='img-overlay'>
+            <Modal.Body className='img-overlay d-flex align-items-center justify-content-center'>
                 <img src={src} alt="carousel" />
                 {/* <button onClick={() => setShow(false)}>X</button> */}
             </Modal.Body>
